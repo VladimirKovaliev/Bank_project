4f1f8bfa2067dccc3aa5b12e90f747f85ac463bb
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-06 16:06:05 +0300

    Добавил функции для работы

---
+++ utils/arrs.py
@@ -0,0 +1,21 @@
+import json
+
+'''Функции для работы с массивами'''
+
+
+def get_id(operation_id):
+    ...
+
+def operation_date(date):
+    ...
+
+def operation_description(description):
+    ...
+
+def operation_state(state):
+    ...
+
+def get_operation_amount(operation_amount):
+    ...
+def get_operation_currentcy(currency):
+    ...
