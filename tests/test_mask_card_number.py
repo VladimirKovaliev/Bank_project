c5a090319baaf80ce017596e50b56d5be2a88d9d
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-07 19:13:34 +0300

    add tests

--- tests/test_mask_card_number.py
+++ tests/test_mask_card_number.py
@@ -1,4 +1,6 @@
-from utils import funcs
+from utils.mask_card_number import mask_card_number
 
 def test_mask_card_number():
-    ...
+    assert mask_card_number('Счет 46363668439560358409') == 'Счет 4636 46** **** 8409'
+    assert mask_card_number('Maestro 6000111122223333') == 'Maestro 6000 60** **** 3333'
+    assert mask_card_number(' 4276380012345678') == ' 4276 42** **** 5678'
